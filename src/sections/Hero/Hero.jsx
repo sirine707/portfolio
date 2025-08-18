eac3a8e667e228a7baf96be7d44a7bb81ad1f146
sirine707 <sirinejnayehh@gmail.com>
2025-08-18 20:21:36 +0200

    fix:updated resume link

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -87,7 +87,7 @@ function Hero() {
               </Button>
               <Button
                 type="secondary"
-                href="https://drive.google.com/file/d/1m4GghIOEV3tjQ-YVNwH8lcc-bBiUm1Bg/view?usp=sharing"
+                href="https://drive.google.com/file/d/1pLaZNPltcHc4pqL24LpwBwibnqIgjD9S/view?usp=sharing"
                 newtab="true"
                 className="cursor-auto"
               >
